dcb7203bb9bf0822df28a90a19b4b2c50154f2b9
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2017-01-06 13:59:22 +0700

    edit

--- src/testing/CobaBintang.java
+++ src/testing/CobaBintang.java
@@ -3,84 +3,70 @@ package testing;
 public class CobaBintang {
 	int i,j;
 	
-	public void trapesium(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void trapesium(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>i;j--){
 				System.out.print(" ");
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pyramidebintang_alignleft(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidebintang_alignleft(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
+			}
+			System.out.print("\n");
+		}	
+	}
+	
+	public void pyramidepagar_alignleft(String a,int jumlah){
+		for(i=1;i<=jumlah;i++){
+			for(j=1;j<=i;j++){
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}				
 	}
 	
-	public String bintang(String a){
-		return a="*";
-	}
-	
-	public void pyramidepagar_alignleft(int jumlah){
-		CobaBintang bintang=new CobaBintang();
-		for(i=1;i<=jumlah;i++){
-			for(j=1;j<=i;j++){
-				System.out.print(bintang.pagar("a"));
-			}
-			System.out.print("\n");
-		}				
-	}
-	
-	public String pagar(String a){
-		return a="#";
-	}
-	
-	public void pyramidebintang_alignright(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidebintang_alignright(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>i;j--){
 				System.out.print(" ");
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pyramidepagar_alignright(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidepagar_alignright(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>i;j--){
 				System.out.print(" ");
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pyramidebintang_aligncenter(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidebintang_aligncenter(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>i;j--){
 				System.out.print(" ");
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=jumlah;j>i;j--){
@@ -90,14 +76,13 @@ public class CobaBintang {
 		}		
 	}
 	
-	public void pyramidepagar_aligncenter(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidepagar_aligncenter(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>i;j--){
 				System.out.print(" ");
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=jumlah;j>i;j--){
@@ -107,14 +92,13 @@ public class CobaBintang {
 		}		
 	}
 	
-	public void pyramidebintang_terbalik(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidebintang_terbalik(String a,int jumlah){
 		for(i=jumlah;i>=1;i--){
 			for(j=i;j<jumlah;j++){
 				System.out.print(" ");
 			}
 			for(j=i;j>=1;j--){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=i;j<jumlah;j++){
@@ -124,14 +108,13 @@ public class CobaBintang {
 		}				
 	}
 	
-	public void pyramidepagar_terbalik(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidepagar_terbalik(String a,int jumlah){
 		for(i=jumlah;i>=1;i--){
 			for(j=i;j<jumlah;j++){
 				System.out.print(" ");
 			}
 			for(j=i;j>=1;j--){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=i;j<jumlah;j++){
@@ -141,94 +124,90 @@ public class CobaBintang {
 		}				
 	}
 	
-	public void pyramidebintang_bolong(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidebintang_bolong(String a,String b,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=jumlah;j>=i;j--){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			for(j=1;j<i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(b);
 			}
 			for(j=1;j<i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(b);
 			}
 			for(j=jumlah;j>=i;j--){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			for(j=jumlah;j>i;j--){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(b);
 			}
 			for(j=jumlah;j>i;j--){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(b);
 			}
 			for(j=1;j<=i;j++){
-				System.out.print(bintang.bintang("a"));
-			}
-			System.out.print("\n");
-		}
-	}
-	
-	public void pyramidepagar_bolong(int jumlah){
-		CobaBintang bintang=new CobaBintang();
-		for(i=1;i<=jumlah;i++){
-			for(j=jumlah;j>=i;j--){
-				System.out.print(bintang.pagar("a"));
-			}
-			for(j=1;j<i;j++){
-				System.out.print(bintang.bintang("a"));
-			}
-			for(j=1;j<i;j++){
-				System.out.print(bintang.bintang("a"));
-			}
-			for(j=jumlah;j>=i;j--){
-				System.out.print(bintang.pagar("a"));
-			}
-			System.out.print("\n");
-		}
-		for(i=1;i<=jumlah;i++){
-			for(j=1;j<=i;j++){
-				System.out.print(bintang.pagar("a"));
-			}
-			for(j=jumlah;j>i;j--){
-				System.out.print(bintang.bintang("a"));
-			}
-			for(j=jumlah;j>i;j--){
-				System.out.print(bintang.bintang("a"));
-			}
-			for(j=1;j<=i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void bintang_kotak(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pyramidepagar_bolong(String a,String b,int jumlah){
 		for(i=1;i<=jumlah;i++){
-			System.out.print(bintang.bintang("a"));
+			for(j=jumlah;j>=i;j--){
+				System.out.print(a);
+			}
+			for(j=1;j<i;j++){
+				System.out.print(b);
+			}
+			for(j=1;j<i;j++){
+				System.out.print(b);
+			}
+			for(j=jumlah;j>=i;j--){
+				System.out.print(a);
+			}
+			System.out.print("\n");
+		}
+		for(i=1;i<=jumlah;i++){
+			for(j=1;j<=i;j++){
+				System.out.print(a);
+			}
+			for(j=jumlah;j>i;j--){
+				System.out.print(b);
+			}
+			for(j=jumlah;j>i;j--){
+				System.out.print(b);
+			}
+			for(j=1;j<=i;j++){
+				System.out.print(a);
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	public void bintang_kotak(String a,int jumlah){
+		for(i=1;i<=jumlah;i++){
+			System.out.print(a);
 			System.out.print(" ");
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pagar_kotak(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pagar_kotak(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
-			System.out.print(bintang.pagar("a"));
+			System.out.print(a);
 			System.out.print(" ");
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			System.out.print("\n");
@@ -237,24 +216,23 @@ public class CobaBintang {
 	
 	public void pyramidebintang_diamond(){
 		CobaBintang bintang=new CobaBintang();
-		bintang.pyramidebintang_aligncenter(5);
-		bintang.pyramidebintang_terbalik(5);
+		bintang.pyramidebintang_aligncenter("*",5);
+		bintang.pyramidebintang_terbalik("*",5);
 	}
 	
 	public void pyramidepagar_diamond(){
 		CobaBintang bintang=new CobaBintang();
-		bintang.pyramidepagar_aligncenter(5);
-		bintang.pyramidepagar_terbalik(5);
+		bintang.pyramidepagar_aligncenter("#",5);
+		bintang.pyramidepagar_terbalik("#",5);
 	}
 	
-	public void bintang_panah(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void bintang_panah(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<i;j++){
 				System.out.print(" ");
 			}
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}	
@@ -263,20 +241,19 @@ public class CobaBintang {
 				System.out.print(" ");
 			}
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pagar_panah(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pagar_panah(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<i;j++){
 				System.out.print(" ");
 			}
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}	
@@ -285,55 +262,53 @@ public class CobaBintang {
 				System.out.print(" ");
 			}
 			for(j=1;j<=jumlah;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void bintang_fullpanah(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void bintang_fullpanah(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=1;j<=1;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}	
 		for(i=jumlah-1;i>=1;i--){
 			for(j=1;j<i;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=1;j<=1;j++){
-				System.out.print(bintang.bintang("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
 	}
 	
-	public void pagar_fullpanah(int jumlah){
-		CobaBintang bintang=new CobaBintang();
+	public void pagar_fullpanah(String a,int jumlah){
 		for(i=1;i<=jumlah;i++){
 			for(j=1;j<i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=1;j<=1;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}	
 		for(i=jumlah-1;i>=1;i--){
 			for(j=1;j<i;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 				System.out.print(" ");
 			}
 			for(j=1;j<=1;j++){
-				System.out.print(bintang.pagar("a"));
+				System.out.print(a);
 			}
 			System.out.print("\n");
 		}
